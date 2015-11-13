4690e8a2b79c686aeeb529db90100574779c9f56
TalkTakesTime <b3nf@hotmail.com>
2015-11-14 12:38:58 +1300

    Remove unused directives

--- GameSuite.GamesTests/ConnectFour/GameTests.cs
+++ GameSuite.GamesTests/ConnectFour/GameTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GameSuite.Games.ConnectFour;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameSuite.Games.ConnectFour.Tests
 {
